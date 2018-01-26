d67179e230c19e9bbaf22238c5a361c4afb39a2e
muhammadzaini0190 <zaini.vijai@gmailcom>
2018-01-26 23:45:31 +0800

    edit nama

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -26,7 +26,7 @@
 						<span class="icon-bar"></span>
 						<span class="icon-bar"></span>
 					</button>
-					<a class="navbar-brand" href="#">APOTEK KIMIA FARMA</a>
+					<a class="navbar-brand" href="#">APOTEK ZAI FARMA</a>
 				</div>
 				<div class="collapse navbar-collapse" id="myNavbar">
 					<ul class="nav navbar-nav">
